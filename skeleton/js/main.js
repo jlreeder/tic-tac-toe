7894c3524468e1f33c90e7b67f7949d230c2bdc9
Jason Reeder <info@jasonreeder.com>
2017-02-27 14:09:06 -0800

    Display Board

--- skeleton/js/main.js
+++ skeleton/js/main.js
@@ -1,6 +1,8 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view.js");
+const Game = require("../../solution/game.js");
 
 $( () => {
-  // Your code here
+  // $("figure");
+  let game = new Game();
+  let view = new View(game, $("figure"));
 });
--- skeleton/js/ttt-view.js
+++ skeleton/js/ttt-view.js
@@ -1,11 +1,24 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.game = game;
+    this.$el = $el;
+    this.setupBoard();
+  }
 
   bindEvents() {}
 
   makeMove($square) {}
 
-  setupBoard() {}
+  setupBoard() {
+    let $ul = $("<ul></ul>");
+
+    for (let i = 0; i < 9; i++) {
+      $ul.append($("<li></li>"));
+    }
+
+    this.$el.append($ul);
+    // return "board";
+  }
 }
 
 module.exports = View;
