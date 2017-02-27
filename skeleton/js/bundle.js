448660194dfb4f4988481a015c9a44984ef1d328
Jason Reeder <info@jasonreeder.com>
2017-02-27 15:06:13 -0800

    Add click functionality

--- skeleton/js/bundle.js
+++ skeleton/js/bundle.js
@@ -93,17 +93,38 @@ class View {
 
   bindEvents() {}
 
-  makeMove($square) {}
+  makeMove($square) {
+    let pos = $square.attr("data");
+    $square.append("X");
+    this.game.playMove(pos);
+  }
 
   setupBoard() {
     let $ul = $("<ul></ul>");
 
     for (let i = 0; i < 9; i++) {
-      $ul.append($("<li></li>"));
+      let $li = $("<li></li>");
+
+      $li.hover(
+        function(){
+          $(this).css("background-color", "yellow");
+        }, function(){
+          $(this).css("background-color", "grey");
+        });
+
+      $li.click( function() {
+        $(this).append($li.attr("data"));
+      });
+
+      let col = i % 3;
+      let row = Math.floor(i / 3);
+
+      $li.attr("data",[col,row]);
+
+      $ul.append($li);
     }
 
     this.$el.append($ul);
-    // return "board";
   }
 }
 
